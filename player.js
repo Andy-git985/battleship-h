54225fcfe8d15a3435f811126e56ba6ca24d0d55
Andy-git985 <andy.yu617@gmail.com>
2022-08-04 19:52:34 -0400

    delete player comments

--- player.js
+++ player.js
@@ -1,28 +1,5 @@
 const Gameboard = require('./gameboard.js');
 
-// const Ship = (length) => {
-//   const hits = [];
-//   const hit = (x) => hits.push(x);
-//   const isSunk = () => hits.length === length;
-//   return { length, hits, hit, isSunk };
-// };
-
-// const Gameboard = (() => {
-//   const board = Array(9).fill('');
-//   const hits = [];
-//   const misses = [];
-//   const placeShip = (ship, coordinate) => {
-//     board[coordinate] = ship;
-//   };
-//   const receiveAttack = (x) => {
-//     return board[x] !== '' ? hits.push(x) : miss.push(x);
-//   };
-//   const checkShips = () => {
-//     return board.every((e) => typeof e === 'string');
-//   };
-//   return { board, hits, misses, placeShip, receiveAttack, checkShips };
-// })();
-
 const Player = () => {
   const prototype = Gameboard;
   const attack = (enemy, coordinate) => enemy.receiveAttack(coordinate);

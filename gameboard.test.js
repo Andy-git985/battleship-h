04e7f67ffa693c7bba1dbfe9c7ca6854116083fe
Andy-git985 <andy.yu617@gmail.com>
2022-08-03 23:04:04 -0400

    gameboard test

--- gameboard.test.js
+++ gameboard.test.js
@@ -1,9 +1,22 @@
 const Ship = require('./ship.js');
-const GameBoard = require('./gameboard.js');
+const Gameboard = require('./gameboard.js');
 
 test('attack one ship sunk test', () => {
-  const ship1 = Ship(1);
-  const board = GameBoard;
-  board.placeShip(ship1, 0);
-  expect(board.board).toStrictEqual();
+  const ship1 = Ship(2);
+  const board = Gameboard;
+  const placeArr = [0, 1];
+  placeArr.forEach((e) => board.placeShip(ship1, e));
+  let attack = 0;
+  if (board.receiveAttack(attack)) {
+    board.board[attack].hit(attack);
+  } else {
+    console.log('miss');
+  }
+  attack = 1;
+  if (board.receiveAttack(attack)) {
+    board.board[attack].hit(attack);
+  } else {
+    console.log('miss');
+  }
+  expect(board.board[attack].isSunk()).toStrictEqual(true);
 });
